affe0c4c81e14c7c2cf7fbac7f71ba7c1c3623d2
Diego <diego.moraes@ead.eduvaleavare.com.br>
2025-07-25 16:50:12 -0300

    commit inicial

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+   apiKey: "AIzaSyCFuW0V1ZJOrpxyi64VMKcHEfIAwvxxpo0",
+    authDomain: "araweb-8d663.firebaseapp.com",
+    projectId: "araweb-8d663",
+    storageBucket: "araweb-8d663.firebasestorage.app",
+    messagingSenderId: "561828300052",
+    appId: "1:561828300052:web:bc3b4d99078b2f1cb26e01",
+    measurementId: "G-HH2LY4NCE6"
+};
+firebase.initializeApp(firebaseConfig);
